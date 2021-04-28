2240aa9590df1a30d7936bd058091bc6cc80fb25
jun-ho-Kim <wnsgh5049@naver.com>
2021-04-28 14:42:38 +0900

    Upload video to NodeJS Server with Multer

--- client/src/components/views/UploadVideoPage/UploadVideoPage.js
+++ client/src/components/views/UploadVideoPage/UploadVideoPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
-import DropZone from "react-dropzone"
+import Dropzone from 'react-dropzone'
+import axios from 'axios'
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -24,7 +25,7 @@ function UploadVideoPage() {
     const [description, setDescription] = useState("");
     const [privacy, setPrivacy] = useState("");
     const [categories, setCategories] = useState("");
-
+    const [filePath, setFilePath] = useState("")
     const handleChangeTitle = (event) => {
         setTitle(event.target.value);
     };
@@ -40,27 +41,53 @@ function UploadVideoPage() {
     const handleChangeCategories = (event) => {
         setCategories(event.currentTarget.value)
     };
+    const onSubmit = () => {
+
+    }
+    const onDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+        console.log(files);
+        axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success) {
+                    console.log("response:", response);
+                    let variable = {
+                        filePath: response.data.filePath,
+                        fileName: response.data.fileName,
+                    }
+                    console.log("variable:", variable);
+                    setFilePath(response.data.filePath)
+                } else {
+                    alert('비디오 업로드를 실패하였습니다.');
+                }
+            });
+    };
     return (
+    <div>
+        <>
         <div style={{maxWidth: '700px', margin: '2rem auto' }}>
             <div style={{textAlign: 'center', marginBottom: '2rem'}}>
                 <Title level={2} >Upload Video</Title>
             </div>
-            <Form onSubmit>
+            <Form onSubmit={onSubmit}>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                    <DropZone 
-                    onDrop
-                    multiple
-                    maxSize
+                    <Dropzone 
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={800000000}
                     >
-                    {({getRootProps, getInputProps}) => (
-                        <div style={{width: '300px', height: '240px', border: '1px solid lightgray',
-                        display: 'flex',alignItems: 'center', justifyContent: 'center'}} {...getRootProps()}>
-                            <Input {...getInputProps()} />
-                            <Icon type="plus" style={{fontSize: '3rem'}} />
-                        </div>
+                     {({ getRootProps, getInputProps }) => (
+                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                            {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                <Icon type="plus" style={{fontSize: '3rem'}} />
+                            </div>
                     )}
-
-                    </DropZone>
+                    </Dropzone>             
                     <div>
                         <img src alt />
                     </div>
@@ -97,7 +124,9 @@ function UploadVideoPage() {
             </Button>
             </Form>
         </div>
+        </>
+    </div>
     )
 }
 
-export default UploadVideoPage
+export default UploadVideoPage;
